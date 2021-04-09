fd46ab7321351039dce08bf11e62c3779ec916b6
Dongho Kim <olik369@gmail.com>
2021-04-10 00:31:11 +0900

    7장까지

---
+++ 07장/chp19.ReadonlyMethod/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace chp19.ReadonlyMethod
+{
+    struct ACSetting
+    {
+        public double currentInCelsius; // 현재 온도(섭씨)
+        public double target;           // 희망 온도
+
+        public readonly double GetFahrenheit()
+        {
+            // 프로그래머의 실수로 구조체내의 필드값을 변경하는 것을 방지하기 위해
+            // readonly를 사용해서 막은 것임 아래와 같이 필드값을 변경하는 코드를
+            // 선언하면 컴파일 단계에서 에러를 띄움
+            //target = currentInCelsius * 1.8 + 32;
+            //return target;
+            double currentInFahrenheit = currentInCelsius * 1.8 + 32;
+            return currentInFahrenheit;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ACSetting acs;
+            acs.currentInCelsius = 25;
+            acs.target = 25;
+
+            WriteLine($"{acs.GetFahrenheit()}");
+            WriteLine($"{acs.target}");
+        }
+    }
+}
---
+++ 07장/chp20.Tuple/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+namespace chp20.Tuple
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            //명명되지 않은 튜플
+            WriteLine("------명명되지 않은 튜플------");
+            var a = ("슈퍼맨", 9999);
+            WriteLine($"{a.Item1},{a.Item2}");  //명명되지 않은 튜플은 기본적으로 
+                                                //Item1,2,3 등의 필드이름으로 저장됨
+            //명명된 튜플
+            WriteLine("------명명된 튜플------");
+            var b = (Name: "김동호", Age: 17); //명명된 인수처럼 호출!
+            WriteLine($"{b.Name}, {b.Age}");
+
+            //분해
+            WriteLine("------분해1------");
+            var (name, age) = b;
+            WriteLine($"{name}, {age}");
+
+            //분해2
+            WriteLine("------분해2------");
+            var (name2, age2) = ("홍길동", 35);
+            WriteLine($"{name2}, {age2}");
+
+            //명명된 튜플 = 명명되지 않은 튜플
+            WriteLine("------명명된 튜플 = 명명되지 않은 튜플------");
+            b = a;
+            WriteLine($"{b.Name}, {b.Age}");    //출력 : 슈퍼맨, 9999
+        }
+    }
+}
