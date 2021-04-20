82f6abb93644f2a92943e1a136409c59472d3eec
Dongho Kim <olik369@gmail.com>
2021-04-20 23:29:16 +0900

    p.418까지.....

---
+++ 11장 일반화 프로그래밍/chp01.CopyingArray/MainApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using System.Collections;
+
+//Generic(일반화 프로그래밍), 메소드뒤에<>를 넣어서 다양한 데이터타입을 받을 수 있게 해줌!!
+namespace chp01.CopyingArray
+{
+    class MainApp
+    {
+        //일반화 메소드
+        static void CopyArray<T>(T[] source,T[] target)
+        {
+            for(int i = 0; i < source.Length; i++)
+            {
+                target[i] = source[i];
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] source = { 1, 2, 3, 4, 5 };
+            int[] target = new int[source.Length];
+
+            CopyArray<int>(source, target);
+
+            foreach (var element in target)
+                WriteLine(element);
+
+            string[] source2 = { "하나", "둘", "셋", "넷", "다섯" };
+            string[] target2 = new string[source2.Length];
+
+            CopyArray<string>(source2, target2);
+
+            foreach (var element in target2)
+                WriteLine(element);
+        }
+    }
+}
---
+++ 11장 일반화 프로그래밍/chp03.ConstraintsOnTypeParameters/MainApp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using static System.Console;
+
+//이건 정말 헬임;; 몇몇개 빼고는 매우 헷갈림
+namespace chp03.ConstraintsOnTypeParameters
+{
+    class StructArray<T> where T : struct
+    {
+        public T[] Array { get; set; }
+        public StructArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+
+    class RefArray<T> where T : class
+    {
+        public T[] Array { get; set; }
+        public RefArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+
+    class Base { }
+    class Derived : Base { }
+    class BaseArray<U> where U : Base
+    {
+        public U[] Array { get; set; }
+        public BaseArray(int size)
+        {
+            Array = new U[size];
+        }
+
+        public void CopyArray<T>(T[] Source) where T : U
+        {
+            Source.CopyTo(Array, 0);
+        }
+    }
+
+    class MainApp
+    {
+        public static T CreateInstance<T>() where T : new()
+        {
+            return new T();
+        }
+        static void Main(string[] args)
+        {
+            StructArray<int> a = new StructArray<int>(3);
+            a.Array[0] = 0;
+            a.Array[1] = 1;
+            a.Array[2] = 2;
+
+            RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
+            b.Array[0] = new StructArray<double>(5);
+            b.Array[1] = new StructArray<double>(10);
+            b.Array[2] = new StructArray<double>(1005);
+
+            BaseArray<Base> c = new BaseArray<Base>(3);
+            c.Array[0] = new Base();
+            c.Array[1] = new Derived();
+            c.Array[2] = CreateInstance<Base>();
+
+            BaseArray<Derived> d = new BaseArray<Derived>(3);
+            d.Array[0] = new Derived(); //Base형식은 여기에 할당할 수 없음!!
+            //d.Array[1] = new Base(); 이런식으로는 불가능하다는 의미임
+            d.Array[1] = CreateInstance<Derived>();
+            d.Array[2] = CreateInstance<Derived>();
+
+            BaseArray<Derived> e = new BaseArray<Derived>(3);
+            e.CopyArray<Derived>(d.Array);
+        }
+    }
+}
